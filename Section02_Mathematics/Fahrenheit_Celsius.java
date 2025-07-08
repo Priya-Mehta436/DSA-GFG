e8383ab3809f40961634ef364bee65971befa450
Priya Mehta <pmehta12122005@gmail.com>
2025-07-09 02:27:22 +0530

    Celsius to Fahrenheit

---
+++ Section02_Mathematics/Fahrenheit_Celsius.java
@@ -0,0 +1,10 @@
+//Given a temperature in celsius C. You have to convert it in Fahrenheit.
+
+package Section02_Mathematics;
+
+public class Fahrenheit_Celsius {
+    static double celciusToFahrenheit(int C) {
+        // code here
+        return C*1.8+32;
+    }
+}
