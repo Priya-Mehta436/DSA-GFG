e3d3a0ed7d0b7215e1114001998a2de3932e64ea
Priya Mehta <pmehta12122005@gmail.com>
2025-07-08 23:48:45 +0530

    Area of Triangle

---
+++ Section02_Mathematics/Addition.java
@@ -0,0 +1,12 @@
+//Given two numbers a and b. Your task is to return the sum of a and b.
+
+package Section02_Mathematics;
+
+public class Addition {
+    // User function Template for Java
+    static int addition(int a, int b) {
+        // code here
+        int sum = a+b;
+        return sum;
+    }
+}
