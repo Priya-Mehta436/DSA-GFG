f43ff0a9b56a7580cb32a9daab29591be6d22df9
Priya Mehta <pmehta12122005@gmail.com>
2025-07-09 01:26:12 +0530

    Number Of Divisors

---
+++ Section02_Mathematics/NumberofDivisors.java
@@ -0,0 +1,19 @@
+//Given an integer n, find the number of divisors of n that are divisible by 3
+
+package Section02_Mathematics;
+
+public class NumberofDivisors {
+    // User function Template for Java
+    static int countDivisors(int n) {
+        // code here
+        int count = 0; 
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                if (i % 3 == 0) {
+                    count++; 
+                }
+            }
+        }
+        return count; 
+    }
+}
