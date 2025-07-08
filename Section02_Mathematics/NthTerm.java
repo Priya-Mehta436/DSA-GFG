263b7d7dd664981d74b66711876065c5ba126b6e
Priya Mehta <pmehta12122005@gmail.com>
2025-07-09 02:12:14 +0530

    Find the nth term

---
+++ Section02_Mathematics/NthTerm.java
@@ -0,0 +1,12 @@
+// Given a series 9, 33, 73, 129...  Find the n-th term of the series.
+
+package Section02_Mathematics;
+
+public class NthTerm {
+    // User function Template for Java
+    static long nthOfSeries(long n) {
+        // code here
+        return 8*n*n+1;
+    }
+}
+
