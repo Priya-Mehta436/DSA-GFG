e2334c41e389de550ccfb3cfa75e6e50a8036a07
Priya Mehta <pmehta12122005@gmail.com>
2025-07-08 19:25:27 +0530

    Sum Of n Natural Numbers

---
+++ Section02_Mathematics/SumofNaturalNumbers.java
@@ -0,0 +1,11 @@
+//Given an integer n, your task is to compute the sum of all natural numbers from 1 to n (inclusive). If n is 0, the sum should be 0.
+
+package Section02_Mathematics;
+
+public class SumofNaturalNumbers {
+    public static int findSum(int n) {
+        // code here
+        return n*(n+1)/2;
+    }
+}
+
