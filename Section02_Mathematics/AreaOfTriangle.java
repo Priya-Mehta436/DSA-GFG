376be5b09b23735667e155031f05047ad22db26d
Priya Mehta <pmehta12122005@gmail.com>
2025-07-08 23:45:00 +0530

    Area of Triangle

---
+++ Section02_Mathematics/AreaOfTriangle.java
@@ -0,0 +1,17 @@
+// Given three sides of a triangle A, B and C in the form of integers. Find the area of the triangle.
+// Note: The triangle may not exist. In such cases, the area is 0.
+package Section02_Mathematics;
+
+public class AreaOfTriangle {
+    double findArea(int A, int B, int C) {
+        // code here
+        if ((A+B>C) && (A+C>B) && (B+C> A) ) {
+            double S = (double) (A+B+C ) / 2.0;
+            double area = Math.sqrt(S*(S-A)*(S-B)*(S-C));
+            return area;
+        } else {
+            return 0.0;
+        }
+    }
+}
+    
