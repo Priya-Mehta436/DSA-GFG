f1385e2494f89c67d4d9df193171e69b65cea27f
Priya Mehta <pmehta12122005@gmail.com>
2025-07-10 12:47:12 +0530

    First Set Bit

---
+++ Section03_BitMagic/Set_Bit.java
@@ -0,0 +1,16 @@
+//Given an integer n. You have to return the position of the first set bit  from the right side in the binary representation of the number. If there is no set bit in the integer N, then return 0 from the function.  Given an integer n. You have to return the position of the first set bit  from the right side in the binary representation of the number. If there is no set bit in the integer N, then return 0 from the function.  
+
+package Section03_BitMagic;
+ 
+public class Set_Bit{
+    public static int getFirstSetBit(int n) {
+        // code here
+        if (n == 0) return 0;
+        int setAt = 1;
+        while ((n & 1) == 0) {
+            n = n >> 1;
+            setAt++;
+        }
+        return setAt;
+    }
+}
