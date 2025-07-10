26f821c8665c4a257fccdea16d2296c22a192c1a
Priya Mehta <pmehta12122005@gmail.com>
2025-07-10 10:14:46 +0530

    Number of 1 bits

---
+++ Section03_BitMagic/Count_Bits.java
@@ -0,0 +1,11 @@
+//Given a positive integer n. Your task is to return the count of set bits.
+
+package Section03_BitMagic;
+
+public class Count_Bits {
+ // User function Template for Java
+    static int setBits(int n) {
+        // code here
+        return Integer.bitCount(n);
+    }
+}
