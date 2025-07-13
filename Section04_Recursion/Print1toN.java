2eedff4f4107559fdeeed8f2e43d1e38fe5eb072
Priya Mehta <pmehta12122005@gmail.com>
2025-07-13 23:11:19 +0530

    Print 1 to N without using loop

---
+++ Section04_Recursion/Print1toN.java
@@ -0,0 +1,18 @@
+  /*
+Given an positive integer n, print numbers from 1 to n without using loops.
+Implement the function printTillN() to print the numbers from 1 to n as space-separated integers.
+*/
+
+package Section04_Recursion;
+
+public class Print1toN  {
+     static void printTillN(int N) {
+        // code here
+        printNumbers(1, N);
+    }
+    static void printNumbers(int current, int N) {
+        if (current > N) return;
+        System.out.print(current + " ");
+        printNumbers(current + 1, N);
+    }
+}
