5945512c4c634d8b0c5535f6b1dc68a025609060
Priya Mehta <pmehta12122005@gmail.com>
2025-07-16 20:36:12 +0530

    Array Insert At End

--- Section05_Array/AtEnd.java
+++ Section05_Array/AtEnd.java
@@ -1,10 +1,10 @@
-//Given an array arr that is not completely filled and a value a, you have to insert the value at the end of the array.
-
 package Section05_Array;
-
 import java.util.ArrayList;
 
 public class AtEnd {
+    //Given an array arr that is not completely filled and a value a, you have to insert the value at the end of the array.
+
+
 
     public void insertAtEnd(ArrayList<Integer> arr, int val) {
         // code here
