0020624b777ccb70bcad1268563fd350636026eb
Priya Mehta <pmehta12122005@gmail.com>
2025-08-01 20:06:40 +0530

    Print GFG n times

---
+++ Section04_Recursion/Gfg.java
@@ -0,0 +1,14 @@
+//Print GFG n times
+
+package Section04_Recursion;
+
+class Solution {
+
+    void printGfg(int N) {
+        // code here
+        String gfg = " GFG";
+        for (int i = 0; i < N; i++) {
+            System.out.print(gfg);
+        }
+    }
+}
