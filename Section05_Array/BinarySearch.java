96673137e18f80397ce3fa17f16333d94ea6b39a
Priya Mehta <pmehta12122005@gmail.com>
2025-08-05 17:17:28 +0530

    Binary Search

---
+++ Section05_Array/BinarySearch.java
@@ -0,0 +1,18 @@
+/*Given a sorted array arr[] and an integer k, find the position(0-based indexing) at which k is present in the array using binary search. If k doesn't exist in arr[] return -1. 
+
+Note: If multiple occurrences are there, please return the smallest index. */
+
+package Section05_Array;
+
+public class BinarySearch {
+    public int binarysearch(int[] arr, int k) {
+        // Code Here
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            if (arr[i] == k){
+                return i;
+            }
+        }
+            return -1;
+    }
+}
