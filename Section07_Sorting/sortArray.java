502095eb3934410ac2d52fdc8a6f7535872f0a3a
Priya Mehta <pmehta12122005@gmail.com>
2025-08-27 19:35:21 +0530

    Sort Array 2

---
+++ Section07_Sorting/sortArray.java
@@ -0,0 +1,11 @@
+// Given an unsorted array arr[] of numbers, sort the array in ascending order.
+
+package Section07_Sorting;
+import java.util.Arrays;
+
+public class sortArray {
+    void sortArr(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+    }
+}
